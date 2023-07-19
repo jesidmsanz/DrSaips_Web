8e44605c2788491d574953073643c791892d24a0
Jesid Moscote Sanchez <68261922+jesidmsanz@users.noreply.github.com>
2023-07-18 20:17:12 -0500

    Update getDataOfOracle.js

--- server/getDataOfOracle.js
+++ server/getDataOfOracle.js
@@ -4,6 +4,7 @@ export const getDataOfOracle = async (query) => {
   try {
     if (query) {
       console.log("query", query);
+      console.log("route", process.env.RUTE_INSTANTCLIENT);
       OracleDB.initOracleClient({ libDir: process.env.RUTE_INSTANTCLIENT });
       // OracleDB.initOracleClient({ libDir: "/var/www/html/websites/DrSaips_Web/instantclient_21_10" });
 
