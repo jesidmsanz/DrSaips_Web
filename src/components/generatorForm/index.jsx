28a3b1067edd1415956fbd4b42b8819d99e24e12
Jesid Moscote Sanchez <68261922+jesidmsanz@users.noreply.github.com>
2023-07-27 01:28:54 -0500

    Update index.jsx

--- src/components/generatorForm/index.jsx
+++ src/components/generatorForm/index.jsx
@@ -102,7 +102,11 @@ export default function GeneratorForm({ user }) {
                 <td>{data[0]?.ACTIVIDAD}</td>
                 <td>{data[0]?.FEC_RECEPCION.split("T")[0]}</td>
                 <td>{data[0]?.FEC_SALIDA.split("T")[0]}</td>
-                <td>{data[0]?.MED_BULT}</td>
+                <td>
+                  {data[0]?.MED_BULT
+                    ? parseFloat(data[0]?.MED_BULT).toFixed(1)
+                    : ""}
+                </td>
                 <td className="text-center">
                   <Button
                     variant="primary"
