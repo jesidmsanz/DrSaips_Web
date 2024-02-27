f0e0a3f67fed275afd2fc2bd7b355bda46b955fe
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-02-27 16:36:38 -0500

    Update conectection.js

--- server/db/conectection.js
+++ server/db/conectection.js
@@ -6,6 +6,6 @@ export const credentialsOracleDb = {
   connectString:
     //"(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = pirasoft.top)(PORT = 1521))(CONNECT_DATA =(SID= xe)))",
     //"(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 52.146.8.64)(PORT = 1521))(CONNECT_DATA =(SID= xe)))",
-    "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521))(CONNECT_DATA =(SID= xe)))",
+    "(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.1.4)(PORT = 1521))(CONNECT_DATA =(SID= xe)))",
   externalAuth: false,
 };
